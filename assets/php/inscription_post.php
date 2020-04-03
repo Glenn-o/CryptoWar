de19dfa284f4614d0cf937bcc2c54e05b701006a
Glenn Oberlé <oberle.glenn@icloud.com>
2020-04-03 16:59:24 +0200

    page_connexion(html)+page_accueil(html)+page_inscription(html&php)

---
+++ assets/php/inscription_post.php
@@ -0,0 +1,67 @@
+<?php
+
+$bdd = new PDO('mysql:host=127.0.0.1:3307;dbname=cryptowar_db', 'root', '');
+
+$pseudo = $_POST['username'];
+$email = $_POST['email'];
+$motDePasse = $_POST['password'];
+$mdp_correspond = "";
+$champ_formulaire = "";
+$inscrit = "";
+$email_existe = "";
+$niveau = 0;
+
+if(!(empty($_POST['username']) && empty($_POST['email']) && empty($_POST['password']))){
+    if ($_POST['password'] != $_POST['password_again']){
+        $mdp_correspond = "Les mots de passe ne correspondent pas !";
+    }
+    else{
+        $stmt = $bdd->prepare("SELECT Email FROM joueur WHERE email=?");
+        $stmt->execute([$email]); 
+        $user = $stmt->fetch();
+        if($user)
+        {
+            $email_existe = "Un compte utilise déjà cette adresse mail";
+        }
+        else{
+            $req = $bdd->prepare('INSERT INTO joueur(Pseudo, Email, Motdepasse, Niveau) VALUES(:pseudo, :email, :motDePasse, :niveau)');
+            $req->execute(array('pseudo'=> $pseudo, 'email'=>$email, 'motDePasse'=>$motDePasse, 'niveau'=> $niveau));
+            $inscrit = "Félicitation, vous êtes inscrit !";
+        }
+    }
+}
+else{
+    $champ_formulaire = "Tous les champs doivent être remplis";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Ubuntu&display=swap" rel="stylesheet">
+</head>
+<body id="body_inscription">
+    <header>
+        <i><p>CryptoWar</p></i>
+    </header>
+    <div id="formulaire_inscription">
+        <h1>S'inscrire</h1>
+        <form action="../php/inscription_post.php" method="POST">
+            <input type="text" id="username" name="username" placeholder="Pseudo">
+            <input type="email" id="email" name="email" placeholder="Email">
+            <input type="password" id="password" name="password" placeholder="Mot de passe">
+            <input type="password" id="password" name="password_again" placeholder="Retapez votre mot de passe">
+            <input type="submit" id="submit_login" name="submit" value="inscription">
+        </form>
+        <p><?php echo $mdp_correspond; ?></p>
+        <p><?php echo $champ_formulaire; ?></p>
+        <p><?php echo $inscrit; ?></p>
+        <p><?php echo $email_existe; ?></p>
+        <a href="../html/connexion.html">Vous avez déjà un compte ?</a>
+    </div>
+</body>
+</html>
